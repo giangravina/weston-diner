e89c54f28ec23ae964fa6e0835aa0295502c547f
Gian <giangravina@gmail.com>
2020-09-02 22:34:32 -0400

    further progress for weston diner page

--- routes/main.js
+++ routes/main.js
@@ -7,4 +7,16 @@ router.get('/', (req, res, next) => {
     res.render('home', data)
 })
 
+router.get('/menu', (req, res, next) => {
+    const data = {}
+
+    res.render('menu', data)
+})
+
+router.get('/contact', (req, res, next) => {
+    const data = {}
+
+    res.render('contact', data)
+})
+
 module.exports = router
